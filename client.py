3a87976b6a658a2cc72e940f8c66faf74299e258
matwys <matwys2000@gmail.com>
2022-02-14 20:49:05 +0100

    better gui

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import os
 import socket
 import sys
 import threading
@@ -10,157 +11,256 @@ ADDR = (IP, PORT)
 SIZE = 128
 FORMAT = "utf-8"
 
-#łączenie z serwerem
+# łączenie z serwerem
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-loginArray = ["mat","pat","ala","ewa","janek","arek"]
 
-zalogowany = 0
-exit = 0
-login = ""
+# Logowanie
+def login():
+	global login_screen
+	login_screen = Tk()
+	login_screen.title("GG")
 
-root = Tk()
-root.title('GG')
+	# rozmiar i pozycja okna
+	w, h = 400, 450
+	global ws , hs
+	ws = login_screen.winfo_screenwidth() # rozmiary ekranu
+	hs = login_screen.winfo_screenheight()
+	x = (ws/2) - (w/2)
+	y = (hs/2) - (h/2)
+	login_screen.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-TOPPINGS = [
-    (loginArray[0], loginArray[0]),
-	(loginArray[1], loginArray[1]),
-	(loginArray[2], loginArray[2]),
-	(loginArray[3], loginArray[3]),
-	(loginArray[4], loginArray[4]),
-    (loginArray[5], loginArray[5]),
-]
+	Label(text="Welcome to the chat!\nPlease enter your login and password below!", bg="gray", width=w, height="4", font=("Calibri", 12)).pack()
+	Label(text="\n\n\n").pack()
+ 
+	global username_verify
+	global password_verify
+ 
+	username_verify = StringVar()
+	password_verify = StringVar()
+ 
+	global username_login_entry
+	global password_login_entry
+ 
+	Label(login_screen, text="Username").pack()
+	username_login_entry = Entry(login_screen, textvariable=username_verify)
+	username_login_entry.pack()
+	Label(login_screen, text="").pack()
+	Label(login_screen, text="Password").pack()
+	password_login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
+	password_login_entry.pack()
+	Label(login_screen, text="").pack()
+	Button(login_screen, text="Login", width=10, height=1, command = login_verify).pack()
+	
+	login_screen.mainloop()
 
-user = StringVar()
-user.set("pat")
 
-for text, topping in TOPPINGS:
-    Radiobutton(root, text=text, variable=user, value=topping).pack(anchor=W)
+# Weryfikacja logowania
+def login_verify():
+	global username1
+	username1 = username_verify.get()
+	password1 = password_verify.get()
+	
+	if (username1 == "" or password1 == ""):
+		return
+	
+	username_login_entry.delete(0, END)
+	password_login_entry.delete(0, END)
 
+	client.send(username1.encode(FORMAT)) # wysyła login do servera
+	data1 = client.recv(SIZE).decode(FORMAT)
+	# odbiera wynik logowania
+	if(data1 == "pl"): # poprawny login
+		client.send(password1.encode(FORMAT)) # wysyła hasło do servera
+		data2 = client.recv(SIZE).decode(FORMAT)
+		if (data2 == "ph"): # poprawne hasło
+			print("Logged in as "+username1+".")
+			main_account()
+		elif (data2 == "nh"): # niepoprawne hasło
+			password_not_recognised()
+	elif(data1 == "j"): # ktoś inny już jest zalogowany pod tym samym loginem
+		user_already_logged_in()
+	elif(data1 == "nl"): # niepoprawny login
+		user_not_found()
+
+
+# Komunikat - niepoprawne hasło
+def password_not_recognised():
+	global password_not_recog_screen
+	password_not_recog_screen = Toplevel(login_screen)
+	password_not_recog_screen.title("GG Login")
+	
+	# rozmiar i pozycja okna
+	w, h = 200, 100
+	x = (ws/2) - (w/2)
+	y = (hs/2) - (h/2)
+	password_not_recog_screen.geometry('%dx%d+%d+%d' % (w, h, x, y))
+	
+	Label(password_not_recog_screen, text="\nInvalid Password\n").pack()
+	Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
+
+
+# Komunikat - nie znaleziono użytkownika
+def user_not_found():
+	global user_not_found_screen
+	user_not_found_screen = Toplevel(login_screen)
+	user_not_found_screen.title("GG Login")
+	
+	# rozmiar i pozycja okna
+	w, h = 200, 100
+	x = (ws/2) - (w/2)
+	y = (hs/2) - (h/2)
+	user_not_found_screen.geometry('%dx%d+%d+%d' % (w, h, x, y))
+	
+	Label(user_not_found_screen, text="\nUser Not Found\n").pack()
+	Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
+	
+	
+# Komunikat - ktoś jest już zalogowany na tym koncie
+def user_already_logged_in():
+	global user_already_logged_in_screen
+	user_already_logged_in_screen = Toplevel(login_screen)
+	user_already_logged_in_screen.title("GG Login")
+	
+	# rozmiar i pozycja okna
+	w, h = 250, 150
+	x = (ws/2) - (w/2)
+	y = (hs/2) - (h/2)
+	user_already_logged_in_screen.geometry('%dx%d+%d+%d' % (w, h, x, y))
+	
+	Label(user_already_logged_in_screen, text="\nSomeone is already logged in\nto this account.\n").pack()
+	Button(user_already_logged_in_screen, text="OK", command=delete_user_already_logged_in_screen).pack()
+
+
+def delete_login():
+	login_screen.destroy()
+ 
+ 
+def delete_password_not_recognised():
+	password_not_recog_screen.destroy()
+ 
+ 
+def delete_user_not_found_screen():
+	user_not_found_screen.destroy()
+	
+	
+def delete_user_already_logged_in_screen():
+	user_already_logged_in_screen.destroy()
+
+
+# Pisanie wiadomości
+def przycisk_wysylania(receiver):
+	INPUT = Input.get("1.0", "end-1c")
+	if(len(INPUT) < 1):
+		return
+	client.send(":send".encode(FORMAT))
+	data = client.recv(SIZE).decode(FORMAT)
+	client.send(receiver.encode(FORMAT))
+	data = client.recv(SIZE).decode(FORMAT)
+	Output.configure(state='normal')
+	if(data=="0"):
+		Output.insert(END, ("Zły login\n\n"))
+		return
+	if(data=="ow"):
+		Output.insert(END, ("Schowek "+receiver+ " jest przepelniony\n\n"))
+		return
+	client.send(INPUT[0:100].encode(FORMAT))
+	data = client.recv(SIZE).decode(FORMAT)
+	Output.insert(END, ("Wiadomość wysłana do "+receiver+ ":\n"+INPUT[0:100]+"\n\n"))
+	Output.configure(state='disabled')
+	Input.delete(1.0, END)
+
+
+# Odbieranie wiadomości
 def odbieranie():
-    while(zalogowany == 0 and exit == 0):
-        time.sleep(1)
-    while(zalogowany == 1 and exit == 0):
-        time.sleep(0.5)
-        data = ":recv"
-        client.send(data.encode(FORMAT))
-        BigData = client.recv(128).decode(FORMAT)
-        if(len(BigData) <= 1):
-            client.send("end".encode(FORMAT))
-            BigData = client.recv(128).decode(FORMAT)
-        else:
-            Output.insert(END, BigData[1:])
-            client.send("check".encode(FORMAT))
-            while(True):
-                BigData = client.recv(128).decode(FORMAT)
-                if(BigData == "false"):
-                    Output.insert(END, "")
-                    break
-                client.send("check".encode(FORMAT))
-                BigData = client.recv(128).decode(FORMAT)
-                Output.insert(END, BigData)
-                client.send("check".encode(FORMAT))
-        
-    print("koniec odbierania")
-
-def przycisk_logowania(data): #logowanie
-    if(len(data)< 1):
-        return
-    global zalogowany
-    if(zalogowany == 1):
-        return
-    client.send(data.encode(FORMAT)) #wysyła login do servera
-    data1 = client.recv(SIZE).decode(FORMAT) #odbiera wynik logowania: 1 - poprawnie zalogowany, j - ktoś inny już jest zalogowany pod tym samym loginem, z - niepoprawny login 
-    if(data1 == "1"): 
-        print("Użytkownik zalogowal sie")
-        Output.delete('1.0', END)
-        Output.insert(END, ("Użytkownik zalogowal sie\n\n"))
-        zalogowany = 1
-        global login
-        login = data
-        myLabel = Label(root, text=login)
-        myLabel.pack()
-    elif(data1 == "j"):
-        print("proszę wybrać innego użytkownika")
-        Output.delete('1.0', END)
-        Output.insert(END, ("Proszę wybrać innego użytkownika\n\n"))
-    elif(data1 == "z"):
-        print("zły login")
-        Output.delete('1.0', END)
-        Output.insert(END, ("Zły login\n\n"))
-
-def przycisk_wysylania(value): #pisanie wiadomości
-    if(len(value)< 1):
-        return
-    global zalogowany
-    if(zalogowany == 0):
-        Output.delete('1.0', END)
-        Output.insert(END, ("Nie jesteś zalogowany\n\n"))
-        return
-    INPUT = inputtxt.get("1.0", "end-1c")
-    if(len(INPUT) < 1):
-        return
-    client.send(":send".encode(FORMAT))
-    data = client.recv(SIZE).decode(FORMAT)
-    client.send(value.encode(FORMAT))
-    data = client.recv(SIZE).decode(FORMAT)
-    if(data=="0"):
-        Output.insert(END, ("Zły login\n\n"))
-        return
-    if(data=="ow"):
-        Output.insert(END, ("Schowek "+value+ " jest przepelniony\n\n"))
-        return
-    client.send(INPUT[0:100].encode(FORMAT))
-    data = client.recv(SIZE).decode(FORMAT)
-    Output.insert(END, ("Wiadomość wysłana do "+value+ ":\n"+INPUT[0:100]+"\n\n"))
-
-def przycisk_odbierania(): #odbieranie wiadomości
-    global zalogowany
-    if(zalogowany == 0):
-        return
-    data = ":recv"
-    client.send(data.encode(FORMAT))
-    BigData = client.recv(128).decode(FORMAT)
-    if(len(BigData) <= 1):
-        client.send("end".encode(FORMAT))
-        BigData = client.recv(128).decode(FORMAT)
-        return
-    Output.insert(END, BigData[1:])
-    client.send("check".encode(FORMAT))
-    while(True):
-        BigData = client.recv(128).decode(FORMAT)
-        if(BigData == "false"):
-            Output.insert(END, "")
-            break
-        client.send("check".encode(FORMAT))
-        BigData = client.recv(128).decode(FORMAT)
-        Output.insert(END, BigData)
-        client.send("check".encode(FORMAT))
-
-x = threading.Thread(target=odbieranie, args=())
-x.start()
+	while(exit == 0):
+		time.sleep(0.3)
+		data = ":recv"
+		client.send(data.encode(FORMAT))
+		BigData = client.recv(128).decode(FORMAT)
+		if(len(BigData) <= 1):
+			client.send("end".encode(FORMAT))
+			BigData = client.recv(128).decode(FORMAT)
+		else:
+			Output.configure(state='normal')
+			Output.insert(END, BigData[1:])
+			client.send("check".encode(FORMAT))
+			while(True):
+				BigData = client.recv(128).decode(FORMAT)
+				if(BigData == "false"):
+					Output.insert(END, "")
+					break
+				client.send("check".encode(FORMAT))
+				BigData = client.recv(128).decode(FORMAT)
+				Output.insert(END, BigData)
+				client.send("check".encode(FORMAT))
+			Output.configure(state='disabled')
 
 
-myButton2 = Button(root, text="logowanie", command=lambda: przycisk_logowania(user.get()))
-myButton2.pack()
-myButton = Button(root, text="send",command=lambda: przycisk_wysylania(user.get()))
-myButton.pack()
 
-inputtxt = Text(root, height = 1,
-                width = 100,
-                bg = "light yellow")
-  
-Output = Text(root, height = 15, 
-                width = 100, 
-                bg = "light cyan")
-  
-inputtxt.pack()
-Output.pack()
-mainloop()
-exit = 1
-x.join()
-data = ":exit"
-client.send(data.encode(FORMAT))
+# Ekran główny - chat
+def main_account():
+	global exit
+	exit = 0
+	delete_login()
+	global main_account_screen
+	main_account_screen = Tk()
+	main_account_screen.title("GG")
+	
+	# rozmiar i pozycja okna
+	w, h = 500, 500
+	x = (ws/2) - (w/2)
+	y = (hs/2) - (h/2)
+	main_account_screen.geometry('%dx%d+%d+%d' % (w, h, x, y))
+	
+	# wątek do odbierania wiadomości
+	odb = threading.Thread(target=odbieranie, args=())
+	odb.start()
+
+	Label(main_account_screen, text="Welcome, "+username1+"!", 
+	      font=("Calibri", 13), bg="gray", width=w, height=2).pack()
+	Label(main_account_screen, text="\n\n").pack()
+	
+	# wczytanie listy użytkowników z pliku userFriends
+	loginArray = []
+	file1 = open('userFriends', 'r')
+	Lines = file1.readlines()
+	for line in Lines:
+		loginArray.append(line.strip())
+	file1.close()
+		
+	# wybieranie odbiorcy wiadomości - rozwijana lista
+	receiver = StringVar(main_account_screen)
+	receiver.set(loginArray[0])
+	options = OptionMenu(main_account_screen, receiver, *loginArray)
+	options.config(width=90, font=('Helvetica', 12))
+	options.pack(side="top")
+	mess = Label(text="Send a message to {}".format(loginArray[0]), font=('Helvetica', 12), fg='gray')
+	mess.pack(side="top")
+	def callback(*args):
+		mess.configure(text="Send a message to {}".format(receiver.get()))
+	receiver.trace("w", callback)
+
+	global Output
+	Output = Text(main_account_screen, height = 15, width = 100, bg = "white")
+	Output.configure(state='disabled')
+	Output.pack()
+	global Input
+	Input = Text(main_account_screen, height = 2, width = 100, bg = "white")
+	Input.pack()
+	sendButton = Button(main_account_screen, text="send", command=lambda: przycisk_wysylania(receiver.get()))
+	sendButton.pack()
+
+	main_account_screen.mainloop()
+	exit = 1
+	odb.join()
+
+
+
+login()
+
+client.send(":exit".encode(FORMAT))
 client.close()
-print(":exit")
+
+
--- server.c
+++ server.c
@@ -7,57 +7,71 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+
+#define MAX_NUM_OF_USERS 20
 #define PORT 4444
 char buffer[128];
 char wbuffer[16];
+int num_of_users;
+
 
 struct user{
     char login[128]; 
     char warunek[128];
     char file[128];
+    char passwd[128];
 };
-    struct user arr_user[6] = {
-                                {"","1.txt","f1.txt"},
-                                {"","2.txt","f2.txt"},
-                                {"","3.txt","f3.txt"},
-                                {"","4.txt","f4.txt"},
-                                {"","5.txt","f5.txt"},
-                                {"","6.txt","f6.txt"}
-                            };
+
+
+struct user arr_user[MAX_NUM_OF_USERS];
+
 
 int ReadLoginsFromFile(){
-    FILE *filePointer;
-    char ch;
-    filePointer = fopen("loginList", "r");
-    if (filePointer == NULL)
-    {
-        printf("File is not available \n");
-        return 1;
-    }
-    else
-    {
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 6; j++){
-                ch = fgetc(filePointer);
-                if( ch == EOF || ch == '\n' || ch == ' ' || j == 5){
-                    arr_user[i].login[j]= '\0';
-                    break;
-                } else {
-                    arr_user[i].login[j]=ch;
-                }
-            }
-        }
-    }
-    fclose(filePointer);
-    printf("Dostępne loginy:\n");
-    for(int i = 0; i < 6; i++){
-        printf("%s\n", arr_user[i].login);
-    }
-    return 0;
+	// Wczytanie loginów i haseł z pliku loginList
+	FILE *filePointer;
+	char ch;
+	filePointer = fopen("loginList", "r");
+	if (filePointer == NULL) {
+		printf("File 'loginList' is not available.\n");
+		return 1;
+	}
+	else {
+		int i = 0, j = 0, s = 0;
+		int log_or_passwd = 0;
+		while ((ch = fgetc(filePointer)) != EOF) {
+			if (ch == ' ') {
+				strcpy(arr_user[i].warunek, strcat(arr_user[i].warunek, "_warunek"));
+				strcpy(arr_user[i].file, strcat(arr_user[i].file, "_file"));
+				log_or_passwd = 1;
+				s = j;
+			} else if (ch == '\n') {
+				i++;
+				j = 0;
+				log_or_passwd = 0;
+			} else if (log_or_passwd == 0) {
+				arr_user[i].login[j] = ch;
+				arr_user[i].warunek[j] = ch;
+				arr_user[i].file[j] = ch;
+				j++;
+			} else if (log_or_passwd == 1) {
+				arr_user[i].passwd[j-s] = ch;
+				j++;
+			}
+		}
+		num_of_users = i;
+	}
+	fclose(filePointer);
+	printf("=== Dostępne loginy i hasła: ===\n");
+	for(int i = 0; i < num_of_users; i++){
+		printf("%s %s\n", arr_user[i].login, arr_user[i].passwd);
+	}
+	printf("=================================\n");
+	return 0;
 }
+
 void MakeFile(){
     FILE * fp;
-    for(int i = 0; i<6;i++){
+    for(int i = 0; i < num_of_users; i++){
         fp = fopen(arr_user[i].warunek, "w");
         fprintf(fp, "%s", "0");
         fclose(fp);
@@ -66,12 +80,14 @@ void MakeFile(){
         fclose(fp);
     }
 }
+
 void Zalogowany0(int n){
     FILE * fp;
     fp = fopen(arr_user[n].warunek, "w");
     fprintf(fp, "%s", "0");
     fclose(fp);
 }
+
 void Zalogowany1(int n){
     FILE * fp;
     fp = fopen(arr_user[n].warunek, "w");
@@ -91,6 +107,7 @@ int CzyZalogowany(int n){
     if(strcmp(wbuffer, "0") == 0) return 0;
     else return 1;
 }
+
 void Dopisz(int nadawca,int odbiorca){
     FILE * fp;
     fp = fopen (arr_user[odbiorca].file, "a");
@@ -98,6 +115,7 @@ void Dopisz(int nadawca,int odbiorca){
     fprintf(fp, "%s\n\n", buffer);
     fclose(fp);
 }
+
 void Odczyt(int odbiorca, int for_i){
     FILE * fp;
     fp = fopen (arr_user[odbiorca].file, "r");
@@ -122,17 +140,13 @@ int WielkoscSchowka(int odbiorca){
     fclose(fp);
     return lengthOfFile;
 }
-void Odbieranie(){
-}
 
-void Wysylanie(){
-}
 
 int main(){
-    if(0<ReadLoginsFromFile()){
-        exit(1);
-    }
-    MakeFile();
+	if(0<ReadLoginsFromFile()){
+		exit(1);
+	}
+	MakeFile();
     
 	int sockfd, ret;
 	 struct sockaddr_in serverAddr;
@@ -156,7 +170,7 @@ int main(){
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(PORT);
 	//serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
 
 	ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 	if(ret < 0){
@@ -182,101 +196,118 @@ int main(){
 		if((childpid = fork()) == 0){
 			close(sockfd);
 
-            //logowanie
-            int id = -1;
-            int zalogowany = 0;
+			// przed zalogowaniem
+			int id = -1;
+			int zalogowany = 0;
 			while(id == -1){
-                bzero(buffer, sizeof(buffer));
+				bzero(buffer, sizeof(buffer));
 				recv(newSocket, buffer, 128, 0);
-                int komunikat = 0;
-                for(int i = 0;i<6;i++){
-                    if( strcmp( buffer, arr_user[i].login) == 0){
-                        if(CzyZalogowany(i)){
-                            send(newSocket, "j", strlen("j"), 0);
-                            komunikat = 1;
-                        } else {
-                            Zalogowany1(i);
-                            id = i;
-                            send(newSocket, "1", strlen("1"), 0);
-                            zalogowany = 1;
-                        }
-                    }
-                }
+				int komunikat = 0;
 				if(strcmp(buffer, ":exit") == 0){
 					printf("Disconnected from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
 					break;
 				}
-                if(id == -1 && komunikat == 0){
-                    bzero(buffer, sizeof(buffer));
-                    send(newSocket, "z", strlen("z"), 0);
+				/* 
+				   LOGOWANIE
+				   Odbiera nazwę użytkownika i wysyła wynik logowania:
+				   j - ktoś inny już jest zalogowany pod tym samym loginem, 
+				   pl - poprawny login, nl - niepoprawny login,
+				   ph - poprawne haslo, nh - niepoprawne hasło;
+				*/
+				for(int i = 0; i < num_of_users; i++){
+					if( strcmp( buffer, arr_user[i].login) == 0){
+						if(CzyZalogowany(i)){
+							send(newSocket, "j", strlen("j"), 0);
+							komunikat = 1;
+						} else {
+							send(newSocket, "pl", strlen("pl"), 0);
+							bzero(buffer, sizeof(buffer));
+							recv(newSocket, buffer, 128, 0);
+							if (strcmp(buffer, arr_user[i].passwd)){
+								send(newSocket, "nh", strlen("nh"), 0);
+								komunikat = 1;
+							} else {
+								Zalogowany1(i);
+								id = i;
+								zalogowany = 1;
+								send(newSocket, "ph", strlen("ph"), 0);
+							}
+						}
+					}
+				}
+				if(id == -1 && komunikat == 0){
+					bzero(buffer, sizeof(buffer));
+					send(newSocket, "nl", strlen("nl"), 0);
 					bzero(buffer, sizeof(buffer));
 				}
 			}
-
-			while(1 == zalogowany){
+			// po zalogowaniu
+			while(zalogowany == 1){
+				bzero(buffer, sizeof(buffer));
 				recv(newSocket, buffer, 128, 0);
 				if(strcmp(buffer, ":exit") == 0){
 					printf("Disconnected from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
-                    Zalogowany0(id);
-                    zalogowany = 0;
+					Zalogowany0(id);
+					zalogowany = 0;
 					break;
 				}
-                else if(strcmp(buffer, ":send") == 0){
+				// WYSYŁANIE WIADOMOŚCI
+				else if(strcmp(buffer, ":send") == 0){
 					send(newSocket, "1", strlen("1"), 0);
-                    int id_odpiorcy = -1;
-                    while(id_odpiorcy == -1){
-                        bzero(buffer, sizeof(buffer));
-                        recv(newSocket, buffer, 128, 0);
-                        for(int i = 0;i<6;i++){
-                            if( strcmp( buffer, arr_user[i].login) == 0){
-                                id_odpiorcy = i;
-                                if(WielkoscSchowka(id_odpiorcy) < 8000){
-                                    send(newSocket, "check", strlen("check"), 0);
-                                    bzero(buffer, sizeof(buffer));
-                                    recv(newSocket, buffer, 128, 0);//dane do zapisu
-                                    Dopisz(id,id_odpiorcy);
-					                send(newSocket, "1", strlen("1"), 0);
-                                    bzero(buffer, sizeof(buffer));
-                                } else {
-                                    send(newSocket, "ow", strlen("ow"), 0);
-                                    bzero(buffer, sizeof(buffer));
-                                }
-                                break;
-                            }
-                        }
-                        if(id_odpiorcy == -1){
-                            send(newSocket, "0", strlen("0"), 0);
-                        }
-                    }
+					int id_odpiorcy = -1;
+					while(id_odpiorcy == -1){
+						bzero(buffer, sizeof(buffer));
+						recv(newSocket, buffer, 128, 0);
+						for(int i = 0; i < num_of_users; i++){
+                            				if( strcmp( buffer, arr_user[i].login) == 0){
+								id_odpiorcy = i;
+								if(WielkoscSchowka(id_odpiorcy) < 8000){
+ 									send(newSocket, "check", strlen("check"), 0);
+									bzero(buffer, sizeof(buffer));
+									recv(newSocket, buffer, 128, 0); //dane do zapisu
+									Dopisz(id,id_odpiorcy);
+									send(newSocket, "1", strlen("1"), 0);
+									bzero(buffer, sizeof(buffer));
+								} else {
+									send(newSocket, "ow", strlen("ow"), 0);
+									bzero(buffer, sizeof(buffer));
+								}
+								break;
+							}
+						}
+						if(id_odpiorcy == -1){
+							send(newSocket, "0", strlen("0"), 0);
+						}
+					}
 				}
-                else if(strcmp(buffer, ":recv") == 0){
-                    int for_i = (WielkoscSchowka(id)-1) /128;
-                    Odczyt(id, 0);
-                    send(newSocket, buffer, strlen(buffer), 0);
-                    bzero(buffer, sizeof(buffer));
-                    recv(newSocket, buffer, 128, 0);//get "check" or "end"
-                    if(strcmp(buffer, "end")==0){
-                        send(newSocket, buffer, strlen(buffer), 0);
-                        bzero(buffer, sizeof(buffer));
-                    } else {
-                        bzero(buffer, sizeof(buffer));
-                        for(int i =1; i<for_i+1;i++){
-                            send(newSocket, "true", strlen("true"), 0);
-                            recv(newSocket, buffer, 128, 0); //get "check"
-                            bzero(buffer, sizeof(buffer));
-                            Odczyt(id, i);
-                            send(newSocket, buffer, strlen(buffer), 0);
-                            bzero(buffer, sizeof(buffer));
-                            recv(newSocket, buffer, 128, 0); //get "check"
-                            bzero(buffer, sizeof(buffer));
-                        }
-                        send(newSocket, "false", strlen("false"), 0);
-                        Wyczysc(id);
-                    }
-                }
+				// ODBIERANIE WIADOMOŚCI
+				else if(strcmp(buffer, ":recv") == 0){
+					int for_i = (WielkoscSchowka(id)-1) /128;
+					Odczyt(id, 0);
+					send(newSocket, buffer, strlen(buffer), 0);
+					bzero(buffer, sizeof(buffer));
+					recv(newSocket, buffer, 128, 0); //get "check" or "end"
+					if(strcmp(buffer, "end")==0){
+						send(newSocket, buffer, strlen(buffer), 0);
+						bzero(buffer, sizeof(buffer));
+					} else {
+						bzero(buffer, sizeof(buffer));
+						for(int i =1; i<for_i+1;i++){
+							send(newSocket, "true", strlen("true"), 0);
+							recv(newSocket, buffer, 128, 0); //get "check"
+							bzero(buffer, sizeof(buffer));
+							Odczyt(id, i);
+							send(newSocket, buffer, strlen(buffer), 0);
+							bzero(buffer, sizeof(buffer));
+							recv(newSocket, buffer, 128, 0); //get "check"
+							bzero(buffer, sizeof(buffer));
+						}
+						send(newSocket, "false", strlen("false"), 0);
+						Wyczysc(id);
+					}
+				}
 			}
 		}
-
 	}
 
 	close(newSocket);
@@ -284,3 +315,4 @@ int main(){
 
 	return 0;
 }
+
